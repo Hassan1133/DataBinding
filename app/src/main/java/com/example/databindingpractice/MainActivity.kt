968f829bde71704b9c47f68ec44be5326d4bafbd
Hassan Idrees <mhassanidrees575@gmail.com>
2023-12-18 19:50:05 +0500

    data binding practice commit

---
+++ app/src/main/java/com/example/databindingpractice/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.databindingpractice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.databindingpractice.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var userObj = User("Hassan")
+
+        binding.user = userObj
+
+    }
+}
---
+++ app/src/main/java/com/example/databindingpractice/User.kt
@@ -0,0 +1,3 @@
+package com.example.databindingpractice
+
+data class User(val userName : String)
